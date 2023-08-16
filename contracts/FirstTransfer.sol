fc1b95f41a589d4a9a749cc22320beec4a13b8dd
SydenX <je.tollenaers@gmail.com>
2023-08-16 22:37:22 +0200

    Updates

--- contracts/FirstTransfer.sol
+++ contracts/FirstTransfer.sol
@@ -2,19 +2,41 @@
 pragma solidity 0.8.21;
 
 contract FirstApproach{
-    address lastPerson;
-    uint balance;
+
+    struct wallet {
+        uint balance;
+        uint timesSent;
+        uint totalSent;
+    }
+
+    mapping(address => wallet) Wallets;
+
+    function getBalance() public view returns(uint){
+        return Wallets[msg.sender].balance;
+    }
+
+    function getTimesSent() public view returns(uint){
+        return Wallets[msg.sender].timesSent;
+    }
+
+    function getTotalSent() public view returns(uint){
+        return Wallets[msg.sender].totalSent;
+    }
 
     receive() external payable {
-        lastPerson = msg.sender;
-        balance = balance+msg.value;
+        Wallets[msg.sender].balance += msg.value;
+        Wallets[msg.sender].timesSent += 1;
+        Wallets[msg.sender].totalSent += msg.value;
     }
 
-    function getLastPerson() public view returns(address){
-        return lastPerson;
+    function withdraw(address payable _to, uint _amount) public {
+        if(Wallets[msg.sender].balance >= _amount){
+            Wallets[msg.sender].balance-=_amount;
+            _to.transfer(_amount);
+        }
     }
 
-    function getBalance() public view returns (uint){
-        return balance;
+    function getTotalBalance() public view returns (uint){
+        return address(this).balance;
     }
 }
